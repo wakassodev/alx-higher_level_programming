e008518807990bf82c57ec75ab2f2f2a865aadc5
wakasso <wakasso42furtif@gmail.com>
2022-05-31 23:29:44 +0400

    second project Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -7,9 +7,9 @@ def fizzbuzz():
     for number in range(1, 101):
         if (number % 3 and number % 5):
             print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
+        elif (number % 3 and number != 5):
             print("%s" % (FIZZ), end=' ')
-        elif (number % 5):
+        elif (number % 5 and number != 3):
             print("%s" % (BUZZ), end=' ')
         else:
             print("%d" % (number), end=' ')
